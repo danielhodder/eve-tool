124b69094061f29690970d2bbb703f9f3e6b870f
Nicholas Hinds <hindsn@gmail.com>
2013-03-31 19:13:07 +1300

    Allow local property overrides not checked into git. Set the default properties to point at mysql

--- eve-site/src/main/java/nz/net/dnh/eve/config/RootConfig.java
+++ eve-site/src/main/java/nz/net/dnh/eve/config/RootConfig.java
@@ -2,17 +2,30 @@ package nz.net.dnh.eve.config;
 
 import org.springframework.context.annotation.*;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 @Configuration
 @ComponentScan(basePackages = { "nz.net.dnh.eve" })
 public class RootConfig {
+	private static final Resource EMPTY_RESOURCE = new ByteArrayResource(new byte[0]);
 	
 	@Bean
 	public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
-		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
-		ppc.setLocation(new ClassPathResource("/persistence.properties"));
+		final PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+		ppc.setLocations(new Resource[] {
+				new ClassPathResource("/persistence.properties"),
+				getOverrideResource() });
 		return ppc;
+	}
+	
+	private static Resource getOverrideResource() {
+		final Resource overrideResource = new ClassPathResource("/local.properties");
+		if (overrideResource.exists()) {
+			return overrideResource;
+		}
+		return EMPTY_RESOURCE;
 	}
 	
 }
