9e0519634de1a7d111bb87fe7a721df79d3f9e97
danielh <danielhodder@gmail.com>
2013-06-05 21:39:26 +1200

    Changed version reader to actually read the version from the build

--- eve-site/src/main/java/nz/net/dnh/eve/web/view/VersionReader.java
+++ eve-site/src/main/java/nz/net/dnh/eve/web/view/VersionReader.java
@@ -1,26 +1,19 @@
 package nz.net.dnh.eve.web.view;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.util.Properties;
+import java.util.Scanner;
 
 public class VersionReader {
 	private final String version;
 
 	public VersionReader() {
 		final InputStream versionResource = getClass().getResourceAsStream(
-				"version.properties");
+				"/version");
 
 		if (versionResource == null) {
 			this.version = "DEVELOPMENT";
 		} else {
-			try {
-				final Properties properties = new Properties();
-				properties.load(versionResource);
-				this.version = properties.getProperty("version");
-			} catch (final IOException e) {
-				throw new RuntimeException(e);
-			}
+			this.version = new Scanner(versionResource).nextLine();
 		}
 	}
 
