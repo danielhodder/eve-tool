e53347c4dbd0b5cb0dd1f66b0746be64ba8d760d
danielh <danielhodder@gmail.com>
2013-12-01 14:01:49 +1300

    Made the EveCentral market update service transactional

--- eve-site/src/main/java/nz/net/dnh/eve/market/eve_central/EveCentralMarketUpdator.java
+++ eve-site/src/main/java/nz/net/dnh/eve/market/eve_central/EveCentralMarketUpdator.java
@@ -15,8 +15,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class EveCentralMarketUpdator {
 	@Autowired
 	private BlueprintService blueprintService;
