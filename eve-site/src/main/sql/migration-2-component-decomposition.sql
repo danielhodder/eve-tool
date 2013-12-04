423d99874f8cb63d73a26202ac756246523e0130
Nicholas Hinds <hindsn@gmail.com>
2013-12-04 23:54:55 +1300

    Add migration script for the database schema

--- eve-site/src/main/sql/migration-2-component-decomposition.sql
+++ eve-site/src/main/sql/migration-2-component-decomposition.sql
@@ -1,27 +1,9 @@
--- NB: Replace `eve-dump` with the database of your eve dump
-
--- Clear out the schema first (this is the reverse order that these objects are defined in this file)
-DROP VIEW IF EXISTS BlueprintSummary, invBlueprintTypes, invCategories, invGroups, invTypes, BlueprintCosts, BlueprintTypeCosts;
-DROP TABLE IF EXISTS Type;
-DROP VIEW IF EXISTS BlueprintTypes, BlueprintTypesRaw, BlueprintSubTypeRequirements;
-DROP TABLE IF EXISTS Blueprint, BlueprintTypeDecomposition;
-DROP FUNCTION IF EXISTS calculate_materials;
+# We need to recreate the function because we changed its parameters, so drop it first
 DROP FUNCTION IF EXISTS calculate_production_time_hours;
+# The BlueprintSummary view is no longer used
+DROP VIEW BlueprintSummary;
 
--- Helper functions from http://wiki.eve-id.net/Equations
 delimiter $$
-CREATE FUNCTION calculate_materials (materialAmount INT(11), baseWasteFactor INT(11), materialEfficiency INT(11))
-  RETURNS INT(11) DETERMINISTIC
-BEGIN
-  # Can't use a double here as we get annoying rounding errors...
-  DECLARE wasteEffect INT;
-  IF materialEfficiency < 0 THEN
-    SET wasteEffect = ROUND(materialAmount * baseWasteFactor / 100 * (1 - materialEfficiency));
-  ELSE
-    SET wasteEffect = ROUND(materialAmount * baseWasteFactor / 100 * (1 / (materialEfficiency + 1)));
-  END IF;
-  RETURN materialAmount + wasteEffect;
-END$$
 
 -- Calculates the production time, in hours, unrounded, for a single run of a blueprint.
 -- To calculate the exact number of hours, multiply this by the number of runs then take the next-highest integer.
@@ -44,41 +26,11 @@ END$$
 
 delimiter ;
 
-CREATE TABLE Blueprint (
-  blueprintTypeID int(11) NOT NULL,
-  numberPerRun int(11) NOT NULL,
-  productionEfficiency int(11) NOT NULL,
-  saleValue decimal(65,2) NOT NULL,
-  materialEfficiency int(11) NOT NULL,
-  lastUpdated TIMESTAMP NOT NULL,
-  automaticallyUpdateSalePrice bit(1) not null default 0,
-  PRIMARY KEY (blueprintTypeID)
-) ENGINE=InnoDB;
-
 CREATE TABLE BlueprintTypeDecomposition (
   blueprintTypeID int(11) NOT NULL,
   materialTypeID int(11) NOT NULL,
   PRIMARY KEY (blueprintTypeID,materialTypeID)
 ) ENGINE=InnoDB;
-
-# Requirements of sub-types are already added, they need to be subtracted so we collect them here
-CREATE VIEW BlueprintSubTypeRequirements AS
-  SELECT
-    bp.blueprintTypeID as blueprintTypeID,
-    itm.materialTypeID as materialTypeID,
-    sum(itm.quantity) as rawQuantity
-  FROM Blueprint bp
-    JOIN `eve-dump`.invBlueprintTypes ibt ON ibt.blueprintTypeID = bp.blueprintTypeID
-    JOIN `eve-dump`.invTypes it ON it.typeID = ibt.blueprintTypeID
-    JOIN `eve-dump`.ramTypeRequirements ram on ram.typeID = it.typeID
-    JOIN `eve-dump`.invTypes ramT ON ram.requiredTypeID = ramT.typeID
-    JOIN `eve-dump`.invGroups ramG ON ramG.groupID = ramT.groupID
-    JOIN `eve-dump`.ramActivities ramA on ramA.activityID = ram.activityID
-    JOIN `eve-dump`.invTypeMaterials itm on itm.typeID = ramT.typeID
-  WHERE
-    ramA.activityName = 'Manufacturing'
-    AND ramG.groupName != 'Tool'
-  GROUP BY bp.blueprintTypeID, itm.materialTypeID;
 
 CREATE VIEW BlueprintTypesRaw AS
   SELECT
@@ -123,8 +75,7 @@ CREATE VIEW BlueprintTypesRaw AS
     ramA.activityName = 'Manufacturing'
     AND ramC.categoryName != 'Skill';
 
-# Materials can appear in both sides of the union in BlueprintTypesRaw, so sum them and group them so they appear as 1 row
-CREATE VIEW BlueprintTypes AS
+ALTER VIEW BlueprintTypes AS
 SELECT
   blueprintTypeID,
   materialTypeID,
@@ -135,16 +86,7 @@ SELECT
 FROM BlueprintTypesRaw
 GROUP BY blueprintTypeID, materialTypeID;
 
-CREATE TABLE Type (
-  typeID int(11) NOT NULL,
-  cost decimal(65,2) NOT NULL,
-  lastUpdated TIMESTAMP NOT NULL,
-  autoUpdate bit(1) not null default 0,
-  PRIMARY KEY (typeID)
-) ENGINE=InnoDB;
-
-
-CREATE VIEW BlueprintTypeCosts AS
+ALTER VIEW BlueprintTypeCosts AS
   select 
     bp.blueprintTypeID AS blueprintTypeID,
     it.typeName AS blueprintName,
@@ -159,7 +101,7 @@ CREATE VIEW BlueprintTypeCosts AS
     JOIN `eve-dump`.invTypes it ON it.typeID = ibt.productTypeID
     JOIN `eve-dump`.invTypes mt ON mt.typeID = bt.materialTypeID;
 
-CREATE VIEW BlueprintCosts AS
+ALTER VIEW BlueprintCosts AS
   select
     bp.blueprintTypeID AS blueprintTypeID,
     # This makes my head hurt, MySQL doesn't return null if there are null values present, so we need to do it ourselves
@@ -176,16 +118,3 @@ CREATE VIEW BlueprintCosts AS
     left outer join BlueprintTypeCosts btc on btc.blueprintTypeID = bp.blueprintTypeID and btc.decomposed = 0
     JOIN `eve-dump`.ramAssemblyLines ral ON ral.assemblyLineID = 1 # They're all the same!
   group by bp.blueprintTypeID;
-
-
-CREATE VIEW invTypes AS
-  SELECT * FROM `eve-dump`.invTypes;
-
-CREATE VIEW invGroups AS
-  SELECT * FROM `eve-dump`.invGroups;
-
-CREATE VIEW invCategories AS
-  SELECT * FROM `eve-dump`.invCategories;
-
-CREATE VIEW invBlueprintTypes AS
-  SELECT invBlueprintTypes.* FROM `eve-dump`.invBlueprintTypes;
