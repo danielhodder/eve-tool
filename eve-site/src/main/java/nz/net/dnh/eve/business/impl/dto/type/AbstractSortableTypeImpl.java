e7646786245f327893af78a73ee98d62a9c93b07
Nicholas Hinds <hindsn@gmail.com>
2013-12-04 23:37:55 +1300

    Include the id when sorting AbstractTypes
    Since we put them in a SortedMap, compareTo is used for equality, so ensure
    that only AbstractTypes which really represent the same database type compare
    as equals.

--- eve-site/src/main/java/nz/net/dnh/eve/business/impl/dto/type/AbstractSortableTypeImpl.java
+++ eve-site/src/main/java/nz/net/dnh/eve/business/impl/dto/type/AbstractSortableTypeImpl.java
@@ -5,6 +5,10 @@ import nz.net.dnh.eve.business.AbstractType;
 public abstract class AbstractSortableTypeImpl implements AbstractType {
 	@Override
 	public int compareTo(final AbstractType o) {
-		return getName().compareTo(o.getName());
+		final int nameComparison = getName().compareTo(o.getName());
+		if (nameComparison != 0)
+			return nameComparison;
+		else
+			return Integer.valueOf(getId()).compareTo(o.getId());
 	}
 }
