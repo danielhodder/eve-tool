56b05d0f886e25458b852d9648a3ace10948bf36
danielh <danielhodder@gmail.com>
2013-12-22 10:58:00 +1300

    Added comments to the ContextBeanExposingView.

--- eve-site/src/main/java/nz/net/dnh/eve/web/view/ContextBeanExposingView.java
+++ eve-site/src/main/java/nz/net/dnh/eve/web/view/ContextBeanExposingView.java
@@ -8,19 +8,35 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.context.support.ContextExposingHttpServletRequest;
 import org.springframework.web.servlet.view.tiles3.TilesView;
 
+/**
+ * Allows views to call other invoke other spring beans using EL.
+ * 
+ * @author Daniel Hodder (danielh)
+ * @see ContextExposingHttpServletRequest
+ */
 public class ContextBeanExposingView extends TilesView {
-	protected HttpServletRequest getRequestToExpose(
-			final HttpServletRequest originalRequest) {
-		return new ContextExposingHttpServletRequest(originalRequest,
-				getWebApplicationContext(), null);
+	/**
+	 * Wraps the HttpServletRequest in a {@link ContextExposingHttpServletRequest} which enables EL expressions to
+	 * access elements in the context.
+	 * 
+	 * @param originalRequest
+	 *            The original request from Spring.
+	 * @return The wrapped request
+	 */
+	protected HttpServletRequest getRequestToExpose(final HttpServletRequest originalRequest) {
+		return new ContextExposingHttpServletRequest(originalRequest, getWebApplicationContext(), null);
 	}
 
+	/**
+	 * Overrides the superclass method to include wrapping the request.
+	 * 
+	 * {@inheritDoc}
+	 * 
+	 * @see #getRequestToExpose(HttpServletRequest)
+	 */
 	@Override
-	protected void renderMergedOutputModel(final Map<String, Object> model,
-			final HttpServletRequest request, final HttpServletResponse response)
-			throws Exception {
-		final HttpServletRequest requestToExpose = getRequestToExpose(request);
-		exposeModelAsRequestAttributes(model, requestToExpose);
-		super.renderMergedOutputModel(model, requestToExpose, response);
+	protected void renderMergedOutputModel(final Map<String, Object> model, final HttpServletRequest request,
+			final HttpServletResponse response) throws Exception {
+		super.renderMergedOutputModel(model, getRequestToExpose(request), response);
 	}
 }
